c3e6e03fc541e14fc24cf6110b69f3ba9cc1723c
Jan Biehl <jpbiehl@googlemail.com>
2023-05-19 11:41:49 +0200

    To string added

--- TiaGenerator.Core/Models/GeneratorConfiguration.cs
+++ TiaGenerator.Core/Models/GeneratorConfiguration.cs
@@ -3,6 +3,8 @@
 // ReSharper disable UnusedAutoPropertyAccessor.Global
 
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace TiaGenerator.Core.Models
 {
@@ -12,5 +14,24 @@ namespace TiaGenerator.Core.Models
 		public string? Description { get; set; }
 		public string? Author { get; set; }
 		public List<GeneratorAction>? Actions { get; set; }
+
+		/// <inheritdoc />
+		public override string ToString()
+		{
+			StringBuilder builder = new();
+
+			builder.AppendLine($"Name: {Name}");
+			builder.Append($"Description: {Description}");
+			builder.Append($"Author: {Author}");
+
+			builder.AppendLine("Actions:");
+
+			foreach (var action in Actions ?? Enumerable.Empty<GeneratorAction>())
+			{
+				builder.AppendLine($"	{action.Type}");
+			}
+
+			return builder.ToString();
+		}
 	}
 }
