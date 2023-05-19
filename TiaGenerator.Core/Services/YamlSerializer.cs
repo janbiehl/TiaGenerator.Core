276734e820eba3f5108f587ba9e710be483234fa
Jan Biehl <jpbiehl@googlemail.com>
2023-05-19 11:03:44 +0200

    Serializer improvements

--- TiaGenerator.Core/Services/YamlSerializer.cs
+++ TiaGenerator.Core/Services/YamlSerializer.cs
@@ -1,15 +1,27 @@
+using System;
+using System.IO;
+using System.Text;
 using TiaGenerator.Core.Interfaces;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
 namespace TiaGenerator.Core.Services
 {
-	public abstract class YamlSerializer
+	public interface ISerializer
 	{
-		public ISerializer Serializer { get; set; } = DefaultSerializer;
-		public IDeserializer Deserializer { get; set; } = DefaultDeserializer;
+		void Serialize<T>(string filePath, T data);
+		void Serialize<T>(Stream stream, T data);
 
-		private static readonly IDeserializer DefaultDeserializer = new DeserializerBuilder()
+		T Deserialize<T>(string filePath);
+		T Deserialize<T>(Stream stream);
+	}
+
+	public class YamlSerializer : ISerializer
+	{
+		public YamlDotNet.Serialization.ISerializer Serializer { get; set; } = DefaultSerializer;
+		public YamlDotNet.Serialization.IDeserializer Deserializer { get; set; } = DefaultDeserializer;
+
+		private static readonly YamlDotNet.Serialization.IDeserializer DefaultDeserializer = new DeserializerBuilder()
 			.WithNamingConvention(UnderscoredNamingConvention.Instance)
 			//.WithAttributeOverride(typeof(GeneratorAction), "Type", new YamlMemberAttribute {Alias = "type"})
 			.WithTagMapping("!CreatePlcAction", typeof(ICreatePlcAction))
@@ -17,12 +29,52 @@ namespace TiaGenerator.Core.Services
 			.WithTagMapping("!CopyProjectAction", typeof(ICopyProjectAction))
 			.Build();
 
-		private static readonly ISerializer DefaultSerializer = new SerializerBuilder()
+		private static readonly YamlDotNet.Serialization.ISerializer DefaultSerializer = new SerializerBuilder()
 			.WithIndentedSequences()
 			//.WithNamingConvention(UnderscoredNamingConvention.Instance)
 			.WithTagMapping("!CreatePlcAction", typeof(ICreatePlcAction))
 			.WithTagMapping("!OpenProjectAction", typeof(IOpenProjectAction))
 			.WithTagMapping("!CopyProjectAction", typeof(ICopyProjectAction))
 			.Build();
+
+		/// <inheritdoc />
+		public void Serialize<T>(string filePath, T data)
+		{
+			if (File.Exists(filePath))
+				throw new InvalidOperationException($"File '{filePath}' already exists.");
+
+			using var fileStream = File.OpenWrite(filePath);
+			using var streamWriter = new StreamWriter(fileStream, Encoding.UTF8);
+
+			Serializer.Serialize(streamWriter, data);
+		}
+
+		/// <inheritdoc />
+		public void Serialize<T>(Stream stream, T data)
+		{
+			using var streamWriter = new StreamWriter(stream, Encoding.UTF8);
+
+			Serializer.Serialize(streamWriter, data);
+		}
+
+		/// <inheritdoc />
+		public T Deserialize<T>(string filePath)
+		{
+			if (!File.Exists(filePath))
+				throw new FileNotFoundException("The file does not exist", filePath);
+
+			using var fileStream = File.OpenRead(filePath);
+			using var streamReader = new StreamReader(fileStream, Encoding.UTF8);
+
+			return Deserializer.Deserialize<T>(streamReader);
+		}
+
+		/// <inheritdoc />
+		public T Deserialize<T>(Stream stream)
+		{
+			using var streamReader = new StreamReader(stream, Encoding.UTF8);
+
+			return Deserializer.Deserialize<T>(streamReader);
+		}
 	}
 }
