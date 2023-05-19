69abd92658b40876d889189693a307cffb1e9723
Jan Biehl <jpbiehl@googlemail.com>
2023-05-19 13:44:14 +0200

    Rearranged parameter

--- TiaGenerator.Core/Interfaces/IDataStore.cs
+++ TiaGenerator.Core/Interfaces/IDataStore.cs
@@ -2,8 +2,8 @@ namespace TiaGenerator.Core.Interfaces
 {
 	public interface IDataStore
 	{
-		public void SetValue<T>(string key, T value) where T : new();
+		public void SetValue<T>(string key, T value) where T : class, new();
 
-		public T? GetValue<T>(string key) where T : new();
+		public T? GetValue<T>(string key) where T : class, new();
 	}
 }
