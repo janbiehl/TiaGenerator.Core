fde0798350c39242013fd9230152768eed680c6d
Jan Biehl <jpbiehl@googlemail.com>
2023-05-20 09:49:40 +0200

    Execute does now return a Task & The action result is now a type not a tuple

--- TiaGenerator.Core/Models/GeneratorAction.cs
+++ TiaGenerator.Core/Models/GeneratorAction.cs
@@ -1,11 +1,25 @@
+using System.Threading.Tasks;
 using TiaGenerator.Core.Interfaces;
 
 namespace TiaGenerator.Core.Models
 {
+	public struct GeneratorActionResult
+	{
+		public ActionResult Result { get; set; }
+
+		public string Message { get; set; }
+
+		public GeneratorActionResult(ActionResult result, string message)
+		{
+			Result = result;
+			Message = message;
+		}
+	}
+
 	public abstract class GeneratorAction
 	{
 		public string? Type { get; set; }
 
-		public abstract (ActionResult result, string message) Execute(IDataStore datastore);
+		public abstract Task<GeneratorActionResult> Execute(IDataStore datastore);
 	}
 }
